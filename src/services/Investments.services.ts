9d5c1fac704d452b268c06d79e49ec5bcc75d3ca
Luciano <oliveiraluck78@gmail.com>
2022-07-16 14:44:41 -0300

    FEAT: Comprar acoes endpoint complete

--- src/services/Investments.services.ts
+++ src/services/Investments.services.ts
@@ -1,44 +1,48 @@
 import { StatusCodes } from 'http-status-codes';
-// import Sequelize from 'sequelize';
-import BuyedStocks from '../database/models/BuyedStocks';
 import Stocks from '../database/models/StocksModel';
-// import seq from '../database/models';
+import seq from '../database/models';
 import INewBuy from '../interfaces/NewBuy.interface';
 import { IResponse } from '../interfaces/Response.interface';
 import IStock from '../interfaces/Stock.interface';
+import BuyedStocksServices from './BuyedStocks.services';
 
-const validateCreate = (stock: IStock | null, qtdeAtivo: number): IResponse => {
-  if (!stock) return { status: StatusCodes.BAD_REQUEST, response: { message: 'A Ação não existe' } };
-  if (qtdeAtivo > stock.quantity) return { status: StatusCodes.BAD_REQUEST, response: { message: 'Quantidade da Ação acima do limite' } };
+const validateCreate = (stockQtd: number, qtdeAtivo: number): IResponse => {
+  if (qtdeAtivo > stockQtd) return { status: StatusCodes.BAD_REQUEST, response: { message: 'Quantidade da Ação acima do limite' } };
   return { status: StatusCodes.OK, response: { message: 'ok' } };
 };
 
-const create = async (newBuy: INewBuy): Promise<IResponse> => {
-  const { codAtivo, qtdeAtivo, codCliente } = newBuy;
-  const result: IStock | null = await Stocks.findByPk(codAtivo);
-  const insert = {
-    userId: codCliente,
-    stockId: codAtivo,
-    quantity: qtdeAtivo,
-  };
-  // const t = new Sequelize.Transaction(seq, {});
-  if (validateCreate(result, qtdeAtivo).status !== 200) return validateCreate(result, qtdeAtivo);
+const getById = async (stockId: number): Promise<IStock> => {
+  const result: IStock | null = await Stocks.findByPk(stockId);
+  if (result === null) throw new Error('Ação não exite');
+  return result;
+};
 
+const create = async (newBuy: INewBuy): Promise<IResponse> => {
+  const { codAtivo, qtdeAtivo } = newBuy;
+  const {
+    price, quantity, name,
+  } = await getById(codAtivo);
+
+  if (validateCreate(quantity, qtdeAtivo)
+    .status !== 200) return validateCreate(quantity, qtdeAtivo);
+
+  const t = await seq.transaction();
   try {
     await Stocks.update(
-      { quantity: (result?.quantity || 100) - qtdeAtivo },
-      { where: { id: codAtivo } },
+      { quantity: quantity - qtdeAtivo },
+      { where: { id: codAtivo }, transaction: t },
     );
 
-    await BuyedStocks
-      .create(insert);
+    await BuyedStocksServices.create(newBuy, t);
+
+    await t.commit();
 
     return {
       status: StatusCodes.OK,
-      response: { ...newBuy },
+      response: { message: `Foi realizada a compra de ${qtdeAtivo} da ação ${name} resultando em um total de R$${qtdeAtivo * (price || 2)}` },
     };
   } catch (e) {
-    console.log(e);
+    await t.rollback();
     return {
       status: StatusCodes.BAD_REQUEST,
       response: { message: 'Algo deu errado' },
