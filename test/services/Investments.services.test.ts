02740a61d1f1b6e9fc316aa5d755b856f6ad7111
Luciano <oliveiraluck78@gmail.com>
2022-07-19 18:16:44 -0300

    FIX: some tests

--- test/services/Investments.services.test.ts
+++ test/services/Investments.services.test.ts
@@ -4,157 +4,159 @@ import sinon from 'sinon';
 import BuyedStocks from '../../src/database/models/BuyedStocks';
 
 import Stocks from '../../src/database/models/StocksModel';
+import BuyedStocksServices from '../../src/services/BuyedStocks.services';
 import Investments from '../../src/services/Investments.services'
 
-describe('Valida se a quantidade para compra esta disponivel no estoque', () => {
-  describe('Quando é suficiente', () => {
-    const payloadStock = 100;
-    const payloadBuyStock = 20;
+describe('Testa o service de Investments', () => {
+  describe('Valida se a quantidade para compra esta disponivel no estoque', () => {
+    describe('Quando é suficiente', () => {
+      const payloadStock = 100;
+      const payloadBuyStock = 20;
 
-    it('Retorna o objeto correto', () => {
-      const { status } = Investments.validateCreate(payloadStock, payloadBuyStock);
-      
-      expect(status).to.be.equal(StatusCodes.OK)
+      it('Retorna o objeto correto', () => {
+        const { status } = Investments.validateCreate(payloadStock, payloadBuyStock);
+        
+        expect(status).to.be.equal(StatusCodes.OK)
+      })
+    })
+
+    describe('Quando não é suficiente', () => {
+      const payloadStock = 30;
+      const payloadBuyStock = 50;
+
+      it('Retorna o objeto correto', () => {
+        const { status, response } = Investments.validateCreate(payloadStock, payloadBuyStock);
+        
+        expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
+        expect(response).to.be.a('object');
+        expect(response).to.contain({ message: 'Quantidade da ação acima do limite'})
+      })
     })
   })
 
-  describe('Quando não é suficiente', () => {
-    const payloadStock = 30;
-    const payloadBuyStock = 50;
+  describe('Valida se o cliente possui a quantidade suficiente para a venda', () => {
+    describe('Quando é suficiente', () => {
+      const payloadBuyed = 100;
+      const payloadBuyStock = 20;
 
-    it('Retorna o objeto correto', () => {
-      const { status, response } = Investments.validateCreate(payloadStock, payloadBuyStock);
-      
-      expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
-      expect(response).to.be.a('object');
-      expect(response).to.contain({ message: 'Quantidade da ação acima do limite'})
+      it('Retorna o objeto correto', () => {
+        const { status } = Investments.validateUpdate(payloadBuyed, payloadBuyStock);
+        
+        expect(status).to.be.equal(StatusCodes.OK)
+      })
+    })
+
+    describe('Quando não é suficiente', () => {
+      const payloadBuyed = 30;
+      const payloadBuyStock = 50;
+
+      it('Retorna o objeto correto', () => {
+        const { status, response } = Investments.validateUpdate(payloadBuyed, payloadBuyStock);
+        
+        expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
+        expect(response).to.be.a('object');
+        expect(response).to.contain({ message: 'Quantidade da ação maior que a possuida'})
+      })
     })
   })
-})
 
-describe('Valida se o cliente possui a quantidade suficiente para a venda', () => {
-  describe('Quando é suficiente', () => {
-    const payloadBuyed = 100;
-    const payloadBuyStock = 20;
-
-    it('Retorna o objeto correto', () => {
-      const { status } = Investments.validateUpdate(payloadBuyed, payloadBuyStock);
-      
-      expect(status).to.be.equal(StatusCodes.OK)
-    })
-  })
-
-  describe('Quando não é suficiente', () => {
-    const payloadBuyed = 30;
-    const payloadBuyStock = 50;
-
-    it('Retorna o objeto correto', () => {
-      const { status, response } = Investments.validateUpdate(payloadBuyed, payloadBuyStock);
-      
-      expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
-      expect(response).to.be.a('object');
-      expect(response).to.contain({ message: 'Quantidade da ação maior que a possuida'})
-    })
-  })
-})
-
-describe('Retorna as informações com base no id do ativo', () => {
-  const payloadId = 1
-  describe('Quando o ativo existe', () => {
-    before(async () => {
-      const execute = {
-        id: 1,
-        name: "ABEV3",
-        price: 14.57,
-        quantity: 99
-      }
-  
-      sinon.stub(Stocks, 'findByPk').resolves(execute as any)
-    })
-  
-    after(async () => {
-      sinon.restore();
-    })
-
-    it('retorna o objeto esperado', async () => {
-      const result = await Investments.getById(payloadId)
-
-      expect(result).to.be.a('object');
-      expect(result).to.contain(
-        {
+  describe('Retorna as informações com base no id do ativo', () => {
+    const payloadId = 1
+    describe('Quando o ativo existe', () => {
+      before(async () => {
+        const execute = {
           id: 1,
           name: "ABEV3",
           price: 14.57,
           quantity: 99
         }
-      )
-    })
-  })
-})
+    
+        sinon.stub(Stocks, 'findByPk').resolves(execute as any)
+      })
+    
+      after(async () => {
+        sinon.restore();
+      })
 
-describe('Envia as informações do ativo', () => {
-  const payloadId = 1 
-  describe('Quando o ativo existe', () => {
-    before(async () => {
-      const execute = {
-        id: 1,
-        name: "ABEV3",
-        price: 14.57,
-        quantity: 99
-      }
-  
-      sinon.stub(Stocks, 'findByPk').resolves(execute as any)
-    })
-  
-    after(async () => {
-      sinon.restore();
-    })
+      it('retorna o objeto esperado', async () => {
+        const result = await Investments.getById(payloadId)
 
-    it('retorna o objeto esperado', async () => {
-      const {status, response} = await Investments.requestById(payloadId)
-      expect(status).to.be.equal(StatusCodes.OK);
-      expect(response).to.be.a('object');
-      expect(response).to.contain({
-        id: 1,
-        name: "ABEV3",
-        price: 14.57,
-        quantity: 99
+        expect(result).to.be.a('object');
+        expect(result).to.contain(
+          {
+            id: 1,
+            name: "ABEV3",
+            price: 14.57,
+            quantity: 99
+          }
+        )
       })
     })
   })
-})
 
-describe('Realiza uma nova compra', () => {
-  const payloadBuy = {
-    codAtivo: 1,
-    qtdeAtivo: 10,
-    codCliente: 1
-  }
+  describe('Envia as informações do ativo', () => {
+    const payloadId = 1 
+    describe('Quando o ativo existe', () => {
+      before(async () => {
+        const execute = {
+          id: 1,
+          name: "ABEV3",
+          price: 14.57,
+          quantity: 99
+        }
+    
+        sinon.stub(Stocks, 'findByPk').resolves(execute as any)
+      })
+    
+      after(async () => {
+        sinon.restore();
+      })
 
-  describe('Quando realiza a compra corretamente', () => {
-    before(async () => {
-      const execute = {
-        id: 1,
-        name: "ABEV3",
-        price: 14.57,
-        quantity: 99
-      };
+      it('retorna o objeto esperado', async () => {
+        const {status, response} = await Investments.requestById(payloadId)
+        expect(status).to.be.equal(StatusCodes.OK);
+        expect(response).to.be.a('object');
+        expect(response).to.contain({
+          CodAtivo: 1,
+          Valor: 14.57,
+          QtdeAtivo: 99
+        })
+      })
+    })
+  })
 
-      sinon.stub(Stocks, 'findByPk').resolves(execute as any);
-      sinon.stub(Stocks, 'update').resolves();
-      sinon.stub(BuyedStocks, 'create').resolves();
-    });
+  describe('Realiza uma nova compra', () => {
+    const payloadBuy = {
+      codAtivo: 1,
+      qtdeAtivo: 10,
+      codCliente: 1
+    }
 
-    after(async () => {
-      sinon.restore();
-    });
+    describe('Quando realiza a compra corretamente', () => {
+      before(async () => {
+        const execute = {
+          id: 1,
+          name: "ABEV3",
+          price: 14.57,
+          quantity: 99
+        };
 
-    it('Retorna o objeto correto', async () => {
-      const { status, response } = await Investments.create(payloadBuy)
+        sinon.stub(Stocks, 'findByPk').resolves(execute as any);
+        sinon.stub(Stocks, 'update').resolves();
+        sinon.stub(BuyedStocksServices, 'create').resolves();
+      });
 
-      expect(status).to.be.equal(StatusCodes.CREATED)
-      expect(response).to.be.an('object');
-      expect(response).to.contain({ message: `Foi realizada a compra de ${payloadBuy.qtdeAtivo} da ação ABEV3 resultando em um total de R$${payloadBuy.qtdeAtivo * (14.57 || 2)}` })
+      after(async () => {
+        sinon.restore();
+      });
+
+      it('Retorna o objeto correto', async () => {
+        const { status, response } = await Investments.create(payloadBuy)
+
+        expect(status).to.be.equal(StatusCodes.CREATED)
+        expect(response).to.be.an('object');
+        expect(response).to.contain(payloadBuy)
+      })
     })
   })
 })
--- test/services/Users.services.test.ts
+++ test/services/Users.services.test.ts
@@ -22,195 +22,196 @@ const returnMock = {
 }
 
 const payloadId: number = 1;
-describe('Retorna o cliente pelo id', () => {
 
-  describe('Quando o cliente nao existe', () => {
-    before(async () => {
-      const execute = null;
+describe('Testa a camada service de Users', () => {
+  describe('Retorna o cliente pelo id', () => {
 
-      sinon.stub(Users, 'findByPk').resolves(execute);
+    describe('Quando o cliente nao existe', () => {
+      before(async () => {
+        const execute = null;
+
+        sinon.stub(Users, 'findByPk').resolves(execute);
+      });
+
+      after(async () => {
+        sinon.restore();
+      });
+
+      it('retorna o response correto', async () => {
+        const { status, response } = await UsersServices.getById(payloadId);
+
+        expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
+        expect(response).to.include({ message: 'Cliente não existe' });
+      });
     });
 
-    after(async () => {
-      sinon.restore();
-    });
+    describe('Quando o cliente exite', () => {
+      before(async () => {
+        const execute = returnMock;
 
-    it('retorna o response correto', async () => {
-      const { status, response } = await UsersServices.getById(payloadId);
+        sinon.stub(Users, 'findByPk').resolves(execute as any);
+      });
 
-      expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
-      expect(response).to.include({ message: 'Cliente não existe' });
+      after(async () => {
+        sinon.restore();
+      });
+
+      it('Retorna o objeto correto', async () => {
+        const { status, response } = await UsersServices.getById(payloadId);
+
+        expect(status).to.be.equal(StatusCodes.OK);
+        expect(response).to.be.a('object');
+        expect(response).to.contain({ id: 2, name: 'Adevani Nascimento' });
+      });
     });
   });
 
-  describe('Quando o cliente exite', () => {
-    before(async () => {
-      const execute = returnMock;
 
-      sinon.stub(Users, 'findByPk').resolves(execute as any);
-    });
+  describe('Varifica se o usuario existe e se o deposito é válido', () => {
+    describe('Quando o usuario existe e o deposito é válido', () => {
+      before(async () => {
+        const execute = returnMock;
 
-    after(async () => {
-      sinon.restore();
-    });
+        sinon.stub(Users, 'findByPk').resolves(execute as any)
+      });
 
-    it('Retorna o objeto correto', async () => {
-      const { status, response } = await UsersServices.getById(payloadId);
+      after(async () => {
+        sinon.restore()
+      });
 
-      expect(status).to.be.equal(StatusCodes.OK);
-      expect(response).to.be.a('object');
-      expect(response).to.contain({ id: 2, name: 'Adevani Nascimento' });
-    });
-  });
-});
+      it('Retorna o objeto correto', async () => {
+        const { status, response } = await UsersServices.checkUser(payloadId, 50, 'deposit');
 
-
-describe('Varifica se o usuario existe e se o deposito é válido', () => {
-  describe('Quando o usuario existe e o deposito é válido', () => {
-    before(async () => {
-      const execute = returnMock;
-
-      sinon.stub(Users, 'findByPk').resolves(execute as any)
-    });
-
-    after(async () => {
-      sinon.restore()
-    });
-
-    it('Retorna o objeto correto', async () => {
-      const { status, response } = await UsersServices.checkUser(payloadId, 50, 'deposit');
-
-      expect(status).to.be.equal(StatusCodes.OK);
-      expect(response).to.be.a('object');
-      expect(response).to.contain({ id: 2, name: 'Adevani Nascimento' });
-    })
-  })
-
-  describe('Quando o usuario não existe e o deposito é válido', () => {
-    before(async () => {
-      const execute = null;
-
-      sinon.stub(Users, 'findByPk').resolves(execute as any)
-    });
-
-    after(async () => {
-      sinon.restore()
-    });
-
-    it('Retorna o objeto correto', async () => {
-      const { status, response } = await UsersServices.checkUser(payloadId, 50, 'deposit');
-
-      expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
-      expect(response).to.be.a('object');
-      expect(response).to.contain({ message: 'Cliente não existe' });
-    })
-  })
-
-  describe('Quando o usuario existe e o deposito é válido', () => {
-    before(async () => {
-      const execute = returnMock;
-
-      sinon.stub(Users, 'findByPk').resolves(execute as any)
-    });
-
-    after(async () => {
-      sinon.restore()
-    });
-
-    it('Retorna o objeto correto', async () => {
-      const { status, response } = await UsersServices.checkUser(payloadId, 0, 'deposito');
-
-      expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
-      expect(response).to.be.a('object');
-      expect(response).to.contain({message : 'Quantidade do deposito não pode ser negativa ou igual a 0'});
-    })
-  })
-})
-
-describe('Realização do deposito', () => {
-  before(async () => {
-    sinon.stub(Users, 'update').resolves();
-  })
-
-  after(async () => {
-    sinon.restore();
-  })
-
-  describe('Realiza o deposito com sucesso', () => {
-    before(async () => {
-      const execute = returnMock
-
-      sinon.stub(Users, 'findByPk').resolves(execute as any)
-    })
-
-    after(async () => {
-      sinon.restore();
-    })
-
-    it('Retorna o objeto correto', async () => {
-      const { status, response } = await UsersServices.deposit(payloadId, 50);
-
-      expect(status).to.be.equal(StatusCodes.OK);
-      expect(response).to.be.an('object');
-      expect(response).to.contain({
-        id: payloadId,
-        name: returnMock.name,
-        newBalance: returnMock.balance + 50
+        expect(status).to.be.equal(StatusCodes.OK);
+        expect(response).to.be.a('object');
+        expect(response).to.contain({ id: 2, name: 'Adevani Nascimento' });
       })
     })
-  })
-})
 
-describe('Realização a retirada', () => {
-  before(async () => {
-    sinon.stub(Users, 'update').resolves();
-  })
+    describe('Quando o usuario não existe e o deposito é válido', () => {
+      before(async () => {
+        const execute = null;
 
-  after(async () => {
-    sinon.restore();
-  })
+        sinon.stub(Users, 'findByPk').resolves(execute as any)
+      });
 
-  describe('Realiza a retirada com sucesso', () => {
-    before(async () => {
-      const execute = returnMock
+      after(async () => {
+        sinon.restore()
+      });
 
-      sinon.stub(Users, 'findByPk').resolves(execute as any)
+      it('Retorna o objeto correto', async () => {
+        const { status, response } = await UsersServices.checkUser(payloadId, 50, 'deposit');
+
+        expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
+        expect(response).to.be.a('object');
+        expect(response).to.contain({ message: 'Cliente não existe' });
+      })
     })
 
-    after(async () => {
-      sinon.restore();
-    })
+    describe('Quando o usuario existe e o deposito é válido', () => {
+      before(async () => {
+        const execute = returnMock;
 
-    it('Retorna o objeto correto', async () => {
-      const { status, response } = await UsersServices.withdrawal(payloadId, 50);
+        sinon.stub(Users, 'findByPk').resolves(execute as any)
+      });
 
-      expect(status).to.be.equal(StatusCodes.OK);
-      expect(response).to.be.an('object');
-      expect(response).to.contain({
-        id: payloadId,
-        name: returnMock.name,
-        newBalance: returnMock.balance - 50
+      after(async () => {
+        sinon.restore()
+      });
+
+      it('Retorna o objeto correto', async () => {
+        const { status, response } = await UsersServices.checkUser(payloadId, 0, 'deposito');
+
+        expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
+        expect(response).to.be.a('object');
+        expect(response).to.contain({message : 'Quantidade do deposito não pode ser negativa ou igual a 0'});
       })
     })
   })
 
-  describe('Cliente não tem o valor necessario', () => {
+  describe('Realização do deposito', () => {
     before(async () => {
-      const execute = returnMock
-
-      sinon.stub(Users, 'findByPk').resolves(execute as any)
+      sinon.stub(Users, 'update').resolves();
     })
 
     after(async () => {
       sinon.restore();
     })
 
-    it('Retorna o objeto correto', async () => {
-      const { status, response } = await UsersServices.withdrawal(payloadId, 5000);
+    describe('Realiza o deposito com sucesso', () => {
+      before(async () => {
+        const execute = returnMock
 
-      expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
-      expect(response).to.be.an('object');
-      expect(response).to.contain({message: 'Saldo insuficiente'})
+        sinon.stub(Users, 'findByPk').resolves(execute as any)
+      })
+
+      after(async () => {
+        sinon.restore();
+      })
+
+      it('Retorna o objeto correto', async () => {
+        const { status, response } = await UsersServices.deposit(payloadId, 50);
+
+        expect(status).to.be.equal(StatusCodes.OK);
+        expect(response).to.be.an('object');
+        expect(response).to.contain({
+          CodCliente: payloadId,
+          Valor: 50
+        })
+      })
+    })
+  })
+
+  describe('Realização a retirada', () => {
+    before(async () => {
+      sinon.stub(Users, 'update').resolves();
+    })
+
+    after(async () => {
+      sinon.restore();
+    })
+
+    describe('Realiza a retirada com sucesso', () => {
+      before(async () => {
+        const execute = returnMock
+
+        sinon.stub(Users, 'findByPk').resolves(execute as any)
+      })
+
+      after(async () => {
+        sinon.restore();
+      })
+
+      it('Retorna o objeto correto', async () => {
+        const { status, response } = await UsersServices.withdrawal(payloadId, 50);
+
+        expect(status).to.be.equal(StatusCodes.OK);
+        expect(response).to.be.an('object');
+        expect(response).to.contain({
+            CodCliente: payloadId,
+            Valor: 50
+        })
+      })
+    })
+
+    describe('Cliente não tem o valor necessario', () => {
+      before(async () => {
+        const execute = returnMock
+
+        sinon.stub(Users, 'findByPk').resolves(execute as any)
+      })
+
+      after(async () => {
+        sinon.restore();
+      })
+
+      it('Retorna o objeto correto', async () => {
+        const { status, response } = await UsersServices.withdrawal(payloadId, 5000);
+
+        expect(status).to.be.equal(StatusCodes.BAD_REQUEST);
+        expect(response).to.be.an('object');
+        expect(response).to.contain({message: 'Saldo insuficiente'})
+      })
     })
   })
 })
