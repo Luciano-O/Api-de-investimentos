9615a92588e643eac5e11090fa6f2412d48a73e8
Luciano <oliveiraluck78@gmail.com>
2022-07-20 01:05:17 -0300

    FIX: Database config and .env archive

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Options } from 'sequelize';
+import { Dialect, Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
@@ -7,7 +7,7 @@ const config: Options = {
   database: process.env.DB_NAME || 'books_api',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
-  dialect: 'mysql',
+  dialect: process.env.DB_DIALECT as Dialect,
 };
 
 export = config;
