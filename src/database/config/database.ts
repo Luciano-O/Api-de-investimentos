1e80a631864ce40238db70e76302b88ba7ef3a78
Luciano <oliveiraluck78@gmail.com>
2022-07-22 02:45:47 -0300

    FIX: Docker Compose

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -4,7 +4,7 @@ import { Dialect, Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'books_api',
+  database: process.env.DB_NAME || 'XP',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: process.env.DB_DIALECT as Dialect,
