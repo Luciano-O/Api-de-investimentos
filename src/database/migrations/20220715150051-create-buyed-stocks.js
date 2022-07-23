5064df2581f60893dcd03d8560df35f9397f8156
Luciano <oliveiraluck78@gmail.com>
2022-07-23 14:48:10 -0300

    FIX: lint

--- src/database/migrations/20220715150051-create-buyed-stocks.js
+++ src/database/migrations/20220715150051-create-buyed-stocks.js
@@ -1,33 +1,31 @@
-'use strict';
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('buyedStocks', {
-      userId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'users',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        primaryKey: true,
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('buyedStocks', {
+    userId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
       },
-      stockId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'stocks',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        primaryKey: true,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      primaryKey: true,
+    },
+    stockId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'stocks',
+        key: 'id',
       },
-      quantity: {
-        type: Sequelize.INTEGER
-      },
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('buyedStocks');
-  }
-};
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      primaryKey: true,
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+    },
+  });
+}
+
+export async function down(queryInterface) {
+  await queryInterface.dropTable('buyedStocks');
+}
