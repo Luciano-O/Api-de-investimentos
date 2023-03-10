5bd6cf8e8270e67cb44875a39bb33b119d85ee54
Luciano <oliveiraluck78@gmail.com>
2023-03-10 02:38:50 -0300

    Deploy fix

--- src/docs/swagger.config.ts
+++ src/docs/swagger.config.ts
@@ -8,7 +8,7 @@ const swaggerConfig = {
     },
     servers: [
       {
-        url: 'https://luciano-pselxp-back-end.herokuapp.com',
+        url: 'http://18.231.1.209:3001',
         description: 'Servidor deployado',
       }, {
         url: `http://localhost:${process.env.PORT}`,
