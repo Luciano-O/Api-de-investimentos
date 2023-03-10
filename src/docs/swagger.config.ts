7c2436d806d03ef8e298b42bf91e8c588f86d6ae
Luciano <oliveiraluck78@gmail.com>
2023-03-10 02:45:44 -0300

    Local server docs remove

--- src/docs/swagger.config.ts
+++ src/docs/swagger.config.ts
@@ -10,9 +10,6 @@ const swaggerConfig = {
       {
         url: 'http://18.231.1.209:3001',
         description: 'Servidor deployado',
-      }, {
-        url: `http://localhost:${process.env.PORT}`,
-        description: 'Servidor Local',
       }],
     components: {
       securitySchemes: {
