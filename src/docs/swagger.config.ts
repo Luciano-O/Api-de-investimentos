72c6407ad60b7e6993a43601f24233ede87ea603
Luciano <oliveiraluck78@gmail.com>
2023-03-10 02:48:23 -0300

    Local server docs return

--- src/docs/swagger.config.ts
+++ src/docs/swagger.config.ts
@@ -10,6 +10,9 @@ const swaggerConfig = {
       {
         url: 'http://18.231.1.209:3001',
         description: 'Servidor deployado',
+      }, {
+        url: `http://localhost:${process.env.PORT}`,
+        description: 'Servidor Local',
       }],
     components: {
       securitySchemes: {
