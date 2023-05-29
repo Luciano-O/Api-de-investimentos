a4e6a8f8930d0c5e6ef0678649c0d4d4279a1bf8
Luciano Oliveira <oliveiraluck78@gmail.com>
2023-05-29 15:34:52 -0300

    FIX: DB env

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -2,12 +2,12 @@ import 'dotenv/config';
 import { Dialect, Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'XP',
-  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
-  dialect: process.env.DB_DIALECT as Dialect || 'mysql',
+  dialect: process.env.DB_DIALECT as Dialect,
 };
 
 export = config;
--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,4 +1,6 @@
 import { Sequelize } from 'sequelize';
-import * as config from '../config/database';
+import 'dotenv/config';
 
-export default new Sequelize(config);
+const { URL }: string = process.env;
+
+export default new Sequelize(URL);
